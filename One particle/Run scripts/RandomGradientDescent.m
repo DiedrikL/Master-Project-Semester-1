2665fbacd6706439ef6a84457c37c0db1c1086ab
Diedrik <diedrik@hotmail.com>
2022-03-29 16:54:12 +0200

    Edit Random gradient descent with comments

--- One particle/Run scripts/RandomGradientDescent.m
+++ One particle/Run scripts/RandomGradientDescent.m
@@ -1,23 +1,26 @@
 % Runs the gradient descent function, several times with random parameters
-% each time
+% each time before displaying the lowest.
 
 % Clear memory and set format for output to screen
 clear
 format short e
 
-% How mana iterations
+% Parameters
 iters = 5;
+learning = 1e-2;
 
 % Determines if momentum is used, and how much.
 % Must be 0 =< Beta < 1
 Beta = 0.5;
 
+
 Results = zeros(iters,3);
 
+% Runs gradient descent iters times
 for n = 1:iters
+    % Initialize the random variables
     epsilon = 12*rand;
     omega = 12*rand+12i*rand;
-    learning = 1e-2;
     
     [epsilon, omega] = GradientDescent(epsilon, omega, learning, Beta)
     difference = MeasureDiff(epsilon, omega)
@@ -29,6 +32,7 @@ end
 
 [minres,minpos] = min(Results(:,3));
 
+disp('\nLowest result is:')
 epsilon = Results(minpos,1)
 omega = Results(minpos,2)
 difference = Results(minpos,3)
