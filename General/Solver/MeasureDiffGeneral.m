603ebbcd679b24ee2fa1b507d00d82486426efab
Diedrik <diedrik@hotmail.com>
2022-05-12 13:12:26 +0200

    Edit to use TimeOptions for time

--- General/Solver/MeasureDiffGeneral.m
+++ General/Solver/MeasureDiffGeneral.m
@@ -1,4 +1,4 @@
-function Diff = MeasureDiffGeneral(H, solutionGate, options)
+function Diff = MeasureDiffGeneral(H, options)
 % Function that measures the difference from a given gate for the
 % Hamiltonian. 
 %
@@ -9,11 +9,12 @@ function Diff = MeasureDiffGeneral(H, solutionGate, options)
 % rotation the rotation to be done to remove the global phase
 arguments
     H function_handle
-    solutionGate Gates.GateInterface = Gates.Hadamard;
-    options.T(1,1) double = 2*pi;
-    options.Tstart(1,1) double = 0;
-    options.Tsize(1,1) double = 500;
+    options.Gate Gates.GateInterface = Gates.Hadamard;
+    options.Time TimeOptions = TimeOptions;
 end
+
+Time = options.Time;
+Gate = options.Gate;
 
 
 % Compare distance for a gate U with a given gate
@@ -23,23 +24,21 @@ psi0 = [1;0];
 psi1 = [0;1];
 
 % Getting the solution for the start positions
-[~, Psi1] = SolveTDSEgeneral(psi0, H, ...
-    T=options.T, Tstart=options.Tstart, Tsize=options.Tsize);
-[~, Psi2] = SolveTDSEgeneral(psi1, H, ...
-    T=options.T, Tstart=options.Tstart, Tsize=options.Tsize);
+[~, Psi1] = SolveTDSEgeneral(psi0, H, Time);
+[~, Psi2] = SolveTDSEgeneral(psi1, H, Time);
 
 % Creating the gate
 U1 = [Psi1(1, end); Psi1(2, end)];
 U2 = [Psi2(1, end); Psi2(2, end)];
 U = [U1, U2];
 
-gate = solutionGate.gate;
+targetGate = Gate.gate;
 
-U = solutionGate.rotation(U);
+U = Gate.rotation(U);
 
 
 
 % Measuring distance
-Diff = norm(U-gate);
+Diff = norm(U-targetGate);
 
 
