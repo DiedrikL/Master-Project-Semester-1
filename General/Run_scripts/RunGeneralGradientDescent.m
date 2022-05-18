42c17a162ca4160b71e49c6a957215911e1dd119
Diedrik <diedrik@hotmail.com>
2022-05-18 02:42:37 +0200

    Edit run scripts to work with Hamiltonian class

--- General/Run_scripts/RunGeneralGradientDescent.m
+++ General/Run_scripts/RunGeneralGradientDescent.m
@@ -1,20 +1,63 @@
 
+% Setup time
 T = 2*pi;
 
-Time = TimeOptions(Tstart = -T, Tend = T);
+%Time = TimeOptions(Tstart = -T, Tpulse = T);
+Time = TimeOptions;
 
-Hamilt = Hamiltonians.SmoothHamiltonianOffsets(Time = Time);
-HGate = Gates.Hadamard;
-
+% Setup parameters
 epsilon = 1;
 omegaX = 0.6;
 omegaY = 0;
 
 Beta = 0.1;
-
 learning = 1e-2;
 
-parameters = [epsilon, omegaX, omegaY, 0, 0, 0];
+% offsets
+x = 0;
+y = 0;
+z = 0;
+
+parameters = [epsilon, omegaX, omegaY, x, y, z];
+
+% Create the hamiltonian
+Hamilt = Hamiltonians.SmoothHamiltonianOffsets(...
+    Time = Time, Parameters = parameters);
+
+% set target gate
+HGate = Gates.Hadamard;
 
 
-[para, result] = GradientDescent.GradientDescent(parameters, Hamilt, HGate, Beta=Beta, learning=learning)
+
+
+% Run gradient descent
+[para, result] = GradientDescent.GradientDescent(...
+    Hamilt, HGate, Beta=Beta, learning=learning)
+
+
+% Plot result of gradient descent
+
+% Set result of gradient descent as parameters
+Hamilt.Parameters = para;
+
+[Time1, Psi1] = SolveTDSEgeneral(psi0, H1);
+
+a = abs(Psi1(1,:)).^2;
+b = abs(Psi1(2,:)).^2;
+
+norm1 = a+b;
+
+% Plot the result
+hold on
+plot(Time1, a)
+
+pause
+clf
+hold on
+plot(Time1, b)
+
+pause
+clf
+hold on
+plot(Time1, norm1)
+
