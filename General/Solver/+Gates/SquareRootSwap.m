8a7d31e1489e4037e3a7f4847a62d3c98f8f2e3f
Diedrik <diedrik@hotmail.com>
2022-08-31 23:08:43 +0200

    Add gate for square root of swap

---
+++ General/Solver/+Gates/SquareRootSwap.m
@@ -0,0 +1,21 @@
+classdef SquareRootSwap < Gates.GateInterface
+    % The gate and rotation for the Hadamard gate
+    
+    methods(Access = public, Static)
+        function rotate = rotation(U)
+            arguments
+                U(4,4) double
+            end
+
+            phi = angle(U(1,1));
+            rotate = exp(-1i*phi)*U;
+        end
+    end
+
+    properties(Constant)
+        gate = [1 0 0 0; 0 0.5*(1+i) 0.5*(1-i) 0;...
+            0 0.5*(1-i) 0.5*(1+i) 0; 0 0 0 1];
+        Psi0 = eye(4);
+
+    end
+end
