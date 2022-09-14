38da0970a872ffdc979a2c96ad762fc6f773995f
Diedrik <diedrik@hotmail.com>
2022-09-14 19:26:52 +0200

    Edit to fix parameter name overlap

--- General/Run_scripts/RunGradientDescentRandom.m
+++ General/Run_scripts/RunGradientDescentRandom.m
@@ -4,14 +4,14 @@ Time = TimeOptions;
 
 % Setup parameters
 range = 10;
-min = 0.1;
+minimum = 0.1;
 repeats = 5;
 learning = 1e-2;
 HGate = Gates.SquareRootSwap;
 
 
 % Result matrices
-parameters = rand(repeats,4).*range+min;
+parameters = rand(repeats,4).*range+minimum;
 result = zeros(repeats, 1);
 
 for n=1:repeats
@@ -22,6 +22,6 @@ for n=1:repeats
     parameters(n,:) = para;
 end
 
-Index = min(result);
+[m, Index] = min(result);
 result(Index)
 parameters(index,:)
