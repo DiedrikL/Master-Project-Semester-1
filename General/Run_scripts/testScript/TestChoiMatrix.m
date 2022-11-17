39ada83264969728c1496dc5abc5b83c0d0390ec
Diedrik <diedrik@hotmail.com>
2022-11-17 12:32:45 +0100

    Edit test script

--- General/Run_scripts/testScript/TestChoiMatrix.m
+++ General/Run_scripts/testScript/TestChoiMatrix.m
@@ -1,6 +1,15 @@
-Hamiltonian = Hamiltonians.LindbladOne;
-Gate = Gates.Hadamard;
 
-MeasureDiffGeneral(Hamiltonian, Gate = Gate);
+epsilon = 2.4210e+00;
+omegaX = 0;
+omegaY = 1.6971e+00;
+
+parameters = [epsilon, omegaX, omegaY];
+
+Hamiltonian = Hamiltonians.LindbladOne(Parameters = parameters, Gamma = 1000);
+Gate = Gates.Hadamard;
+MeasureDiffGeneral(Hamiltonian, Gate = Gate)
+
+SimpleHam = Hamiltonians.SimpleHamiltonian(Parameters = parameters);
+MeasureDiffGeneral(SimpleHam, Gate = Gate)
 
 
