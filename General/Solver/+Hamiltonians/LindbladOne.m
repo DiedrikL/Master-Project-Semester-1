b4788438e052588832d2bbc455814871f0d7c836
Diedrik <diedrik@hotmail.com>
2022-11-17 12:32:26 +0100

    Edit to use full matrix operator to simplify use

--- General/Solver/+Hamiltonians/LindbladOne.m
+++ General/Solver/+Hamiltonians/LindbladOne.m
@@ -1,9 +1,9 @@
 classdef LindbladOne < Hamiltonians.HamiltonianInterface
     properties
         Time TimeOptions
-        Gamma double
+        Gamma double {mustBeReal}
         Parameters
-        Rho (1,1) {mustBeInteger, mustBeInRange(Rho, 1,4)} = 1;
+%         Rho (1,1) {mustBeInteger, mustBeInRange(Rho, 1,4)} = 1;
     end
     
     properties(Dependent)
@@ -27,12 +27,10 @@ classdef LindbladOne < Hamiltonians.HamiltonianInterface
             % Input validation and default values
             arguments
                 options.Time TimeOptions = TimeOptions;
-                options.Gamma double {mustBeNonzero} = 1;
+                options.Gamma double {mustBeReal} = 1;
                 options.Parameters(1,3) double {mustBeReal} = ones(1,3);
-                options.Rho = 1;
                 options.Measure = Measure.ChoiFidelity;
             end
-            this.Rho = options.Rho;
             this.Time = options.Time;
             this.Gamma = options.Gamma;
             this.Parameters = options.Parameters;
@@ -57,24 +55,28 @@ classdef LindbladOne < Hamiltonians.HamiltonianInterface
             omegaX = this.Parameters(1,2);
             omegaY = this.Parameters(1,3);
             gamma = this.Gamma;
-            rho = sparse(2,2);
-            rho(this.Rho) = 1;
+%             rho = sparse(2,2);
+%             rho(this.Rho) = 1;
 
             % Setup parameters
-            B1 = @(t) omegaX*sin(gamma*t);
-            B2 = @(t) 1i*omegaY*sin(gamma*t);
-            V = @(t) B1(t)+B2(t);
+            B1 = @(t) omegaX*sin(t);
+            B2 = @(t) 1i*omegaY*sin(t);
+            V = @(t) B1(t)-B2(t);
 
-            % Creating hamiltonian
-            H = @(t) [-epsilon/2 V(t); V(t)' epsilon/2];
-            a = [1; 0]*[0 1];
-            A = a'*a;
-
-            % creatin components of the Lindbladian
-            lindblad = @(t) H(t)*rho - rho*H(t) - ...
-                1i*gamma*(A*rho + rho*A - 2*a*rho*a');
+%             % Creating hamiltonian
+%             H = @(t) [-epsilon/2 V(t); V(t)' epsilon/2];
+%             a = [1; 0]*[0 1];
+%             A = a'*a;
 
             % creatin components of the Lindbladian
+%             lindblad = @(t) H(t)*rho - rho*H(t) - ...
+%                 1i*gamma*(A*rho + rho*A - 2*a*rho*a');
+
+            lindblad = @(t) ...
+                    [0 -V(t)' V(t) 2i*gamma;...
+                    -V(t) -epsilon-1i*gamma 0 V(t);...
+                    V(t)' 0 epsilon-1i*gamma -V(t)';...
+                    0 V(t)' -V(t) -2i*gamma];
 
 
         end
@@ -120,14 +122,5 @@ classdef LindbladOne < Hamiltonians.HamiltonianInterface
             this.Parameters = para;
         end
 
-                function this = set.Rho(this, rho)
-            
-            arguments
-                this Hamiltonians.LindbladOne
-                rho
-            end
-            
-            this.Rho = rho;
-        end
     end
 end
