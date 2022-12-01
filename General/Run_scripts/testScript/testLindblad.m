197bd495a81d5aaba0eb11f5ae805b14f93177d7
Diedrik <diedrik@hotmail.com>
2022-12-01 08:48:47 +0100

    Add script to test the lindbladian

---
+++ General/Run_scripts/testScript/testLindblad.m
@@ -0,0 +1,36 @@
+
+epsilon = 5;
+omegaX = 4;
+omegaY = 2;
+% epsilon = 2.4210e+00;
+% omegaX = 3.1426e-16;
+% omegaY = 1.6971e+00;
+gamma = 0;
+
+parameters = [epsilon, omegaX, omegaY];
+PsiL = [1; 0; 0; 0];
+PsiH = [1;0];
+
+Lindbladian = Hamiltonians.LindbladOne(Parameters = parameters, Gamma = gamma);
+% Gate = Gates.Hadamard;
+Gate = Gates.TestGateOne;   
+[~, Lindblad] = SolveTDSEgeneral(PsiL, Lindbladian);
+L = Lindblad(:, end)
+
+SimpleHam = Hamiltonians.SimpleHamiltonian(Parameters = parameters);
+[~, Simple] = SolveTDSEgeneral(PsiH, SimpleHam);
+S = Simple(:, end);
+Solution = S*S'
+
+% FindU(Hamiltonian)
+% Unitary = FindU(SimpleHam)
+
+index = 2;
+for n = 1:index
+    for m = 1:index
+        Rho = sparse(m, n, 1, index, index);
+        RhoVector = reshape(Rho,[],1);
+        [~, solutionMatrix] = SolveTDSEgeneral(RhoVector, Lindbladian);
+        lindbladSolution = reshape(solutionMatrix(:,end), index, index)
+    end
+end
