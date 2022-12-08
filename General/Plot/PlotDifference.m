84d13aae79a50332e1a6cf6a1429b1ef153a7b74
Diedrik <diedrik@hotmail.com>
2022-12-08 04:54:39 +0100

    Edit to add parameter values

--- General/Plot/PlotDifference.m
+++ General/Plot/PlotDifference.m
@@ -1,16 +1,32 @@
+% Script that plots the difference of a Lindbladian from a Hamiltonian for
+% different strength of intereference, given by gamma.
+
 % parameters, should give 0 for the given gate
-epsilon = 5;
-omegaX = 4;
-omegaY = 2;
-parameters = [epsilon, omegaX, omegaY];
+
+% Parameters for TestGate
+% epsilon = 5;
+% omegaX = 4;
+% omegaY = 2;
+
+% Parameters for Hadamard
+epsilon = 2.4210e+00;
+omegaX = 3.1426e-16;
+omegaY = -1.6971e+00;
+% result = 1.2130e-09;
+
+% Parameters for Hadamard
+% epsilon = 2.6561;
+% omegaX = 0.1953;    
+% omegaY = 7.9577;
+% result = 2.0797e-04;
+
+
 
 
 % The gate to target
-Gate = Gates.TestGateOne;
+% Gate = Gates.TestGateOne;
+Gate = Gates.Hadamard;
 
-% epsilon = 2.4210e+00;
-% omegaX = 3.1426e-16;
-% omegaY = 1.6971e+00;
 
 % variables
 N = 1e2;
@@ -19,8 +35,10 @@ goal = 0.2;
 deltaGamma = goal/N;
 gamma = 0:deltaGamma:goal;
 gamma(end) = [];
-Choi = ones(1,N);
 
+% Setting up arrays
+Choi = ones(1,N);
+parameters = [epsilon, omegaX, omegaY];
 
 % Setting up the hamiltonians
 Lindbladian = Hamiltonians.LindbladOne(Parameters = parameters);
