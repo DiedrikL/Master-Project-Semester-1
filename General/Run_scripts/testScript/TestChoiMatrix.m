6a0b6e4dc78a0b8623662100ad714c87dbe67ae3
Diedrik <diedrik@hotmail.com>
2023-01-19 09:19:02 +0100

    Renamed variable name

--- General/Run_scripts/testScript/TestChoiMatrix.m
+++ General/Run_scripts/testScript/TestChoiMatrix.m
@@ -5,11 +5,11 @@ omegaY = 2;
 % epsilon = 2.4210e+00;
 % omegaX = 3.1426e-16;
 % omegaY = -1.6971e+00;
-gamma = 0;
+lambda = 0.01;
 
 parameters = [epsilon, omegaX, omegaY];
 
-Lindbladian = Hamiltonians.LindbladOne(Parameters = parameters, Gamma = gamma);
+Lindbladian = Hamiltonians.LindbladOne(Parameters = parameters, Lambda = lambda);
 % Gate = Gates.Hadamard;
 Gate = Gates.TestGateOne;
 Choi = MeasureDiffGeneral(Lindbladian, Gate = Gate)
