157b8cb4c635e16cbb981ec67b7c6760094f174f
Diedrik <diedrik@hotmail.com>
2023-01-26 14:29:03 +0100

    Edit parameters

--- General/Plot/PlotLambda.m
+++ General/Plot/PlotLambda.m
@@ -4,7 +4,7 @@ Time = TimeOptions;
 
 % Setup parameters
 steps = 15;
-N = 3*steps;
+N = 2*steps;
 learning = 1e-3;
 measure = Measure.ChoiFidelity;
 
@@ -24,19 +24,21 @@ tresh3 = 1e0;
 lambda1 = linspace(0, tresh1, steps);
 lambda2 = linspace(tresh1, tresh2, steps);
 lambda3 = linspace(tresh2, tresh3, steps);
-lambda = [lambda1, lambda2, lambda3];
+lambda = [lambda1, lambda2];
 
 startvalue = [epsilon, omegaX, omegaY];
 
 % Gate to optimize over
 % HGate = Gates.TestGateOne;
-HGate = Gates.TestGateOne;
+HGate = Gates.GateOfOne;
 HGate.gate
 
 
 % create result matrices
 parameters = zeros(N,3);
 result = ones(N, 1);
+referenceValue = ones(N,1);
+
 
 % Setup for waitbar
 bar = PoolWaitbar(N);
@@ -46,16 +48,20 @@ afterEach(q, @(value)bar.updateBarValue(value));
 parfor n=1:N
     Hamilt = Hamiltonians.LindbladOne(Time=Time, Parameters = startvalue, Lambda = lambda(n));
     Hamilt.Measure = measure;
+    referenceValue(n) = MeasureDiffGeneral(Hamilt, Gate = HGate);
     [parameters(n,:), result(n)] = GradientDescent.GradientDescent(...
     Hamilt, ...
     HGate, ...
     learning=learning)   
+
     
     send(q, result(n));
 end
 
 % plot result
 plot(lambda, result)
+hold on
+plot(lambda, referenceValue, 'r')
 
 lambda = transpose(lambda);
 
