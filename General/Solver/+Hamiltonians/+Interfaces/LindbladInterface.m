708abfa6c570bcacd88795a0cd0de8fd6f5ef45b
Diedrik <diedrik@hotmail.com>
2023-02-08 10:32:25 +0100

    Add interface for lindblad equations

---
+++ General/Solver/+Hamiltonians/+Interfaces/LindbladInterface.m
@@ -0,0 +1,30 @@
+classdef (Abstract) LindbladInterface < Hamiltonians.Interfaces.HamiltonianInterface
+    
+    properties(Access=public)
+        Gamma double {mustBeReal} = 0;
+    end
+
+    methods
+        function this = LindbladInterface(options)
+            arguments
+                options.Measure Measure = Measure.ChoiFidelity;
+                options.Gamma double = 0;
+
+            end
+
+            this.Measure = options.Measure;
+            this.Gamma = options.Gamma;
+        end
+
+
+        function this = set.Gamma(this, Gamma)
+            % Set the lambda used by this hamiltonian
+            arguments
+                this Hamiltonians.Interfaces.LindbladInterface
+                Gamma(1,1) double {mustBeReal}
+            end 
+            
+            this.Gamma = Gamma;
+        end
+    end
+end
