3caaff5c25db51018d4f27d5615ef3b5dafdc27b
Diedrik <diedrik@hotmail.com>
2023-02-08 10:45:51 +0100

    Add enum for solvers

---
+++ General/Solver/+HamiltSettings/Solvers.m
@@ -0,0 +1,16 @@
+classdef Solvers
+    properties
+        solver function_handle 
+    end
+    
+    methods
+        function con = Solvers(solver)
+            con.solver = solver;
+        end
+    end
+
+    enumeration
+        Crank_Nicolson(@(psi,hamilt) SolveTDSEgeneral(psi,hamilt))
+        Runge_Kutta(@(rho, hamilt) Runge_kutta(psi, hamilt))
+    end
+end
