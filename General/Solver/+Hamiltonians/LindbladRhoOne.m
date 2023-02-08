0be77ec5269b05d52021b42a3180d984716bdb59
Diedrik <diedrik@hotmail.com>
2023-02-08 12:37:06 +0100

    Add script for Lindblad equation without extracting the values

---
+++ General/Solver/+Hamiltonians/LindbladRhoOne.m
@@ -0,0 +1,73 @@
+classdef LindbladRhoOne < Hamiltonians.Interfaces.LindbladInterface
+    properties(Constant)
+        matrixSize = 2;
+    end
+
+    
+    methods
+        function this = LindbladRhoOne(options)
+            % A matrix derived from the lindblad equation
+            % Takes the optional name value parameters Rho, Time and Gamma
+            % 
+            % Time is the TimeOptions used by this hamiltonian
+            %
+            % Gamma is an optional parameter with default value of 0. It
+            % scales how large the effect reducing the state of the system
+            % towards basis state [1;0], it is the interference experienced
+            % by the system.
+            
+            % Input validation and default values
+            arguments
+                options.Time TimeOptions = TimeOptions;
+                options.Gamma double {mustBeReal} = 0;
+                options.Parameters(1,3) double {mustBeReal} = ones(1,3);
+                options.Measure = Measure.ChoiFidelity;
+                options.Solver = HamiltSettings.Solvers.Crank_Nicolson;
+            end
+            this.Time = options.Time;
+            this.Gamma = options.Gamma;
+            this.Parameters = options.Parameters;
+            this.Measure = options.Measure;
+            this.Solver = options.Solver;
+        end
+        
+        function lindblad = createHamiltonian(this)
+            % Creates a Hamiltonian with the parameters provided and the
+            % values stored in this instance of the class
+            %
+            % parameters must be in the form of a (1,3) double vector, with
+            % real numbers. It contain epsilon, omegaX and omegaY.
+            %
+
+
+
+            % Input validation
+            arguments
+                this Hamiltonians.LindbladOne
+            end
+            
+            epsilon = this.Parameters(1,1);
+            omegaX = this.Parameters(1,2);
+            omegaY = this.Parameters(1,3);
+            Gamma = this.Gamma;
+%             rho = sparse(2,2);
+%             rho(this.Rho) = 1;
+
+            % Setup parameters
+            B1 = @(t) omegaX*sin(t);
+            B2 = @(t) 1i*omegaY*sin(t);
+            Omega = @(t) B1(t)+B2(t);
+
+
+            % Creating hamiltonian
+            H = @(t) [-epsilon/2 Omega; conj(Omega) epsilon/2]
+            a = [1; 0]*[0 1]
+            A = a'*a;
+
+            % creatin components of the Lindbladian
+            lindblad = @(t,rho) H(t)*rho - rho*H(t) - 1i*gamma*(A*rho + rho*A -2.*a*rho*a');
+        end
+
+
+    end
+end
