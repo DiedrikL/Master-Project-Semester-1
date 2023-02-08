b65cf81bef3bca3cb0970df5a26a1cb6db26d540
Diedrik <diedrik@hotmail.com>
2023-02-08 12:32:32 +0100

    Renamed folder to avoid name conflict

--- General/Solver/+SolveFunk/Runge_Kutta.m
+++ General/Solver/+SolveFunk/Runge_Kutta.m
@@ -1,26 +1,14 @@
 function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
-    
     % Solves the schrödinger equation with the Runge Kutta method for a
     % specified Hamiltonian and startvalues Rho.
     
-    
     % Input validation
     arguments
-        Rho (:,:) double
-        Hamiltonian Hamiltonians.HamiltonianInterface;
+        Rho double
+        Hamiltonian Hamiltonians.Interfaces.HamiltonianInterface;
     end
 
-
-    % Function for step in Runge Kutta
-    function step = rk_algorithm(t, rho)
-        k1 = expm(-1i*H(t)*dt)*rho; 
-        k2 = expm(-1i*H(t+dt/2)*dt)*(rho+dt*k1/2);
-        k3 = expm(-1i*H(t+dt/2)*dt)*(rho+dt*k2/2);
-        k4 = expm(-1i*H(t+dt)*dt)*(rho+dt*k3);
-
-        step = rho + 1/6*(k1 + 2*k2 + 2*k3 + k4)*dt;
-    
-    end
+    disp('test')
     
     % Get parameterized hamiltonian 
     H = Hamiltonian.createHamiltonian;
@@ -29,13 +17,21 @@ function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
     % Set timelength
     dt = Hamiltonian.TimeStep;
     tVector = Hamiltonian.TimeVector;
-    %tVector(end) = [];
     
     % Find sizes
     leng = length(tVector);
     psiHeight = Hamiltonian.matrixSize;
 
+    % Function for step in Runge Kutta
+    function step = rk_algorithm(t, rho)
+        k1 = expm(-1*H(t)*dt)*rho; 
+        k2 = expm(-1*H(t+dt/2)*dt)*(rho+dt*k1/2);
+        k3 = expm(-1*H(t+dt/2)*dt)*(rho+dt*k2/2);
+        k4 = expm(-1*H(t+dt)*dt)*(rho+dt*k3);
+
+        step = rho + 1/6*(k1 + 2*k2 + 2*k3 + k4)*dt;
     
+    end
 
     Y = zeros(psiHeight, leng);
     
