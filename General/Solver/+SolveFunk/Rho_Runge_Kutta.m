a9090ea1682b59b5fb0bc04ec034112fdfe9297f
Diedrik <diedrik@hotmail.com>
2023-02-09 12:09:04 +0100

    Edit to use right matrix size and moved lines for clarity

--- General/Solver/+SolveFunk/Rho_Runge_Kutta.m
+++ General/Solver/+SolveFunk/Rho_Runge_Kutta.m
@@ -1,4 +1,4 @@
-function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
+function [tVector, Solution] = Rho_Runge_Kutta(Rho, Hamiltonian)
     
     % Solves the schrödinger equation with the Runge Kutta method for a
     % specified Hamiltonian and startvalues Rho.
@@ -7,25 +7,24 @@ function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
     % Input validation
     arguments
         Rho (:,:) double
-        Hamiltonian Hamiltonians.HamiltonianInterface;
+        Hamiltonian Hamiltonians.Interfaces.LindbladInterface;
     end
 
 
     % Function for step in Runge Kutta
     function step = rk_algorithm(t, rho)
-        k1 = expm(-1i*H(t, rho)*dt)*rho; 
-        rho2 = (rho+dt*k1/2);
+        k1 = H(t, rho); 
 
-        k2 = expm(-1i*H(t+dt/2, rho2)*dt)*rho2;
-        rho3 = (rho+dt*k2/2);
+        rho2 = rho+dt*k1/2;
+        k2 = H(t+dt/2, rho2);
 
-        k3 = expm(-1i*H(t+dt/2, rho3)*dt)*rho3;
-        rho4 = (rho+dt*k3);
+        rho3 = rho+dt*k2/2;
+        k3 = H(t+dt/2, rho3);
 
-        k4 = expm(-1i*H(t+dt)*dt, rho4)*rho4;
-
+        rho4 = rho+dt*k3;
+        k4 = H(t+dt, rho4);
+        
         step = rho + 1/6*(k1 + 2*k2 + 2*k3 + k4)*dt;
-    
     end
     
     % Get parameterized hamiltonian 
@@ -35,7 +34,6 @@ function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
     % Set timelength
     dt = Hamiltonian.TimeStep;
     tVector = Hamiltonian.TimeVector;
-    %tVector(end) = [];
     
     % Find sizes
     leng = length(tVector);
@@ -43,11 +41,11 @@ function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
 
     
 
-    Y = zeros(psiHeight, leng);
+    Y = zeros(psiHeight, psiHeight, leng);
     
     for n = 1:leng
         Rho = rk_algorithm(tVector(n),Rho);
-        Y(:,n) = Rho;
+        Y(:,:,n) = Rho;
     end
     
     Solution = Y;
