bdb2cf4d200f25aee28e414b35071fcb22b6a45b
Diedrik <diedrik@hotmail.com>
2023-02-24 11:10:41 +0100

    edit start values

--- General/Run_scripts/RunGeneralGradientDescentTwo.m
+++ General/Run_scripts/RunGeneralGradientDescentTwo.m
@@ -1,18 +1,23 @@
 
 % Setup time
-Time = TimeOptions;
+Time = TimeOptions(Tsize = 5e2);
 
 % Setup parameters
 % epsilon = 9.7661;
 % omegaX = 9.8733;
-% omegaY = 2.1526;
-% u = 0.3129;
- epsilon = 2.0163e+00   
- omegaX = 4.1951e+00;
- omegaY = 9.3115e+00;
- uz = 2.5358e+00;
- ux = 3.3929e+00;
- uy = 6.8453e-01;
+% % omegaY = 2.1526;
+% % u = 0.3129;
+%  epsilon = 2.0163e+00   
+%  omegaX = 4.1951e+00;
+%  omegaY = 9.3115e+00;
+%  uz = 2.5358e+00;
+%  ux = 3.3929e+00;
+%  uy = 6.8453e-01;
+epsilon = 0.9994;
+omegaX = 0.9994;
+omegaY = 0.9994;
+u = 0.9994;
+gamma = 1e-1;
                
 % Beta = 0.1;
 learning = 1e-3;
@@ -23,16 +28,17 @@ x = 0;
 y = 0;
 z = 0;
 
-parameters = [epsilon, omegaX, omegaY, uz, ux, uy];
+parameters = [epsilon, omegaX, omegaY, u];
 % parameters = (rand(1,6) - 0.5)*0.5 + 1;
 % Create the hamiltonian
 % Hamilt = Hamiltonians.TwoParticleInteractionHamiltonian(Time=Time, Parameters = parameters);
-Hamilt = Hamiltonians.TwoParticleMultiInteractionHamiltonian( ...
+Hamilt = Hamiltonians.LindbladRhoTwo(...
+    Gamma = gamma,...
     Time=Time, ...
     Parameters = parameters);
 
 % set target gate
-HGate = Gates.OnesGate;
+HGate = Gates.GateOfOneTwoParticles;
 
 
 
@@ -49,16 +55,16 @@ HGate = Gates.OnesGate;
 
 % Set result of gradient descent as parameters
 Hamilt.Parameters = para;
-psi0 = HGate.Psi0(:,1);
+% psi0 = HGate.Psi0(:,1);
 
-[Time1, Psi1] = SolveTDSEgeneral(psi0, Hamilt);
+% [Psi1] = SolveTDSEgeneral(psi0, Hamilt);
 
-a = abs(Psi1(1,:)).^2;
-b = abs(Psi1(2,:)).^2;
-c = abs(Psi1(3,:)).^2;
-d = abs(Psi1(4,:)).^2;
+% a = abs(Psi1(1,:)).^2;
+% b = abs(Psi1(2,:)).^2;
+% c = abs(Psi1(3,:)).^2;
+% d = abs(Psi1(4,:)).^2;
 
-norm1 = a+b+c+d;
+% norm1 = a+b+c+d;
 
 % Plot the result
 % hold on
