63252ebfb6c98dbe4f969f545352aaf12d5f5349
Diedrik <diedrik@hotmail.com>
2023-02-24 11:08:35 +0100

    Edit to remove unused paths

--- General/Solver/SolveTDSEgeneral.m
+++ General/Solver/SolveTDSEgeneral.m
@@ -1,19 +1,10 @@
-function [tVector, Solution] = SolveTDSEgeneral(psi, Hamiltonian)
+function [Solution] = SolveTDSEgeneral(psi, Hamiltonian)
     arguments
         psi double
         Hamiltonian Hamiltonians.Interfaces.HamiltonianInterface
     end
 
     funkSolver = Hamiltonian.Solver;
-
-    switch funkSolver
-        case HamiltSettings.Solvers.Crank_Nicolson
-
-        case HamiltSettings.Solvers.Runge_Kutta
-
-        otherwise
-            error('Unknown solver')
-    end
     
-    [tVector, Solution] = funkSolver.solver(psi, Hamiltonian);
+    [Solution] = funkSolver.solver(psi, Hamiltonian);
 end
