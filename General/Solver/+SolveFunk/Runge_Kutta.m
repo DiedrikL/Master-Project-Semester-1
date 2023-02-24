0fbc07125c0a78431e8907231265d309c5762d3a
Diedrik <diedrik@hotmail.com>
2023-02-24 11:05:54 +0100

    Edit to remove unused variables and fix bug

--- General/Solver/+SolveFunk/Runge_Kutta.m
+++ General/Solver/+SolveFunk/Runge_Kutta.m
@@ -1,4 +1,4 @@
-function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
+function [Solution] = Runge_Kutta(Rho, Hamiltonian)
     % Solves the schrödinger equation with the Runge Kutta method for a
     % specified Hamiltonian and startvalues Rho.
     
@@ -24,10 +24,11 @@ function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
 
     % Function for step in Runge Kutta
     function step = rk_algorithm(t, rho)
-        k1 = expm(-1*H(t)*dt)*rho; 
-        k2 = expm(-1*H(t+dt/2)*dt)*(rho+dt*k1/2);
-        k3 = expm(-1*H(t+dt/2)*dt)*(rho+dt*k2/2);
-        k4 = expm(-1*H(t+dt)*dt)*(rho+dt*k3);
+        k1 = (H(t)*dt)*rho; 
+        k2 = (H(t+dt/2)*dt)*(rho+dt*k1/2);
+        k3 = (H(t+dt/2)*dt)*(rho+dt*k2/2);
+        k4 = (H(t+dt)*dt)*(rho+dt*k3);
+
 
         step = rho + 1/6*(k1 + 2*k2 + 2*k3 + k4)*dt;
     
@@ -37,8 +38,7 @@ function [tVector, Solution] = Runge_Kutta(Rho, Hamiltonian)
     
     for n = 1:leng
         Rho = rk_algorithm(tVector(n),Rho);
-        Y(:,n) = Rho;
     end
     
-    Solution = Y;
+    Solution = Rho;
 end
