07c7845cea2a34e28c12d20866b57922d7de1fd6
Diedrik <diedrik@hotmail.com>
2023-02-28 01:33:21 +0100

    Edit indents

--- General/Run_scripts/testScript/solveForIndex.m
+++ General/Run_scripts/testScript/solveForIndex.m
@@ -1,9 +1,10 @@
 function choiPart = solveForIndex(Lindblad, rhoSize, index)
-arguments
-    Lindblad Hamiltonians.Interfaces.HamiltonianInterface
-    rhoSize(1,1) int8
-    index(1,1) int8
-end
+    arguments
+        Lindblad Hamiltonians.Interfaces.HamiltonianInterface
+        rhoSize(1,1) int8
+        index(1,1) int8
+    end
+    
     Rho0 = zeros(rhoSize);
     Rho0(index) = 1;
     Rho = SolveFunk.SolveForRho(Lindblad, Rho0, rhoSize);
