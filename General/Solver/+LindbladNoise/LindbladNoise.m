a9e4131c1f4a8fc8944840d797268e1113708a8f
Diedrik <diedrik@hotmail.com>
2023-03-07 13:04:10 +0100

    Edit class to use more general setup, and have contructor with input noise

--- General/Solver/+LindbladNoise/LindbladNoise.m
+++ General/Solver/+LindbladNoise/LindbladNoise.m
@@ -1,31 +1,43 @@
 classdef LindbladNoise
-
-    properties(Constant)
-        L = HamiltSettings.Noise.getL;
-        D = HamiltSettings.Noise.getD;
+    % Class with methods to construct two particle noise for a noise given
+    % during construction
+    properties(SetAccess = immutable)
+        L (4,4,4) double
+        D (4,4) double
+        Gamma (4,1) double
     end
 
 
-    methods(Static)
-        function noise = getNoise(rho)
+    methods(Access = public)
+        function this = LindbladNoise(NoiseType)
+            % Constructor that takes the noise before doing partial
+            % calculations on the noise
+            arguments
+                NoiseType LindbladNoise.TwoParticleNoiseInterface
+            end
+            this.L = NoiseType.getL;
+            this.D = NoiseType.getD;
+            this.Gamma = NoiseType.getGamma;
+        end
+        
+        function noise = getNoise(this, rho)
             %Returns the noise for a given rho
 
             arguments
+                this LindbladNoise.LindbladNoise
                 rho(4,4) double
             end
 
             L_sum = zeros(4,4);
-            L = HamiltSettings.LindbladNoise.L;
-            D = HamiltSettings.LindbladNoise.D;
 
             for n = 1:4
-                L_n = L(:,:,n);
-                gamma_n = HamiltSettings.Noise.getGamma_n(n);
+                L_n = this.L(:,:,n);
+                gamma_n = this.Gamma(n);
                 L_sum = L_sum + gamma_n .* L_n * rho * L_n';
 
             end
             
-            noise = D*rho+rho*D -2*L_sum;
+            noise = this.D*rho+rho*this.D - 2*L_sum;
         end
     end
 
