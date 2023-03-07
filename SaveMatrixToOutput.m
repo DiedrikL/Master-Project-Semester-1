6317f3fa98d50fffbe4316f23725eeaf93b9fdbc
Diedrik <diedrik@hotmail.com>
2023-03-07 13:01:09 +0100

    Edit save matrix to include time and date

--- SaveMatrixToOutput.m
+++ SaveMatrixToOutput.m
@@ -1,8 +1,14 @@
-function SaveMatrixToOutput(matrix, name)
+function SaveMatrixToOutput(matrix, nameTmp)
 % Saves a given matrix with a given filename in the output folder, creating
 % the output folder as necesary
     saveDirectory = 'Outputs';
-    fileName = fullfile(saveDirectory, name);
+    d = string(datetime('now'));
+    redName = regexprep(nameTmp, '[0.]', '')
+
+    formatSpec = string(redName)+'_%s';
+    name = sprintf(formatSpec, d);
+    nameFormat = regexprep(name, '[\s:.]', '_');
+    fileName = fullfile(saveDirectory, nameFormat);
 
     if ~exist(saveDirectory, 'dir')
         mkdir(saveDirectory);
