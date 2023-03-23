3d808dcbeac5ba7858ce2f49011a7d52e5aa3c23
Diedrik <diedrik@hotmail.com>
2023-03-23 06:29:51 +0100

    Add script to plot 3d plot

---
+++ General/Plot/PlotRoom.m
@@ -0,0 +1,59 @@
+% Plots the difference with respect to a gate for different parameters
+
+% Clear memory and set format for output to screen
+clear
+format short e
+
+% Parameter size and resolution
+L = 6;
+N = 80;
+
+% Measure and Gate
+measure = Measure.AvgFidelity;
+Gate = Gates.Hadamard;
+
+% Static parameters
+epsilon = 2.4210e+00;
+omegaX = 3.1426e-16;
+omegaY = 1.6971e+00;
+lambda = 0.0;
+
+% Setup space
+parameter = linspace(-L/2,L/2,N);  
+
+Room = zeros(N,N);
+
+parfor m = 1:N
+    m
+    % Hamiltonian
+    Hamilt = Hamiltonians.SimpleHamiltonian;
+    Hamilt.Measure = measure;
+
+    for n = 1:N
+        para = [parameter(n), omegaX, parameter(m)];
+        Hamilt.Parameters = para;
+
+        Room(m,n) = MeasureDiffGeneral(Hamilt, Gate = Gate);
+
+    end
+end
+
+% Plot room
+figure
+pcolor(parameter, parameter, Room)
+xlabel('Epsilon')
+ylabel('Omega Y')
+colorbar
+
+figure
+surf(parameter, parameter, Room)
+xlabel('Epsilon')
+ylabel('Omega Y')
+
+% Finds and prints lowest value with parameters
+[M, I] = min(Room,[],'all','linear');
+[row, col] = ind2sub(N,I);
+LowestPara1 = parameter(row)
+LowestPara2 = parameter(col)
+
+LowestValue = Room(row, col)
