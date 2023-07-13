ae3b4c4f2106961f9082970e9ef5da661522a2dc
Diedrik <diedrik@hotmail.com>
2023-07-13 15:14:33 +0200

    Add parameters to script

--- General/Plot/PlotRoom.m
+++ General/Plot/PlotRoom.m
@@ -1,32 +1,41 @@
-% Plots the difference with respect to a gate for different parameters
+%% Plots the difference with respect to a gate for different parameters
 
 % Clear memory and set format for output to screen
 clear
 format short e
 
 % Parameter size and resolution
-L = 8;
-N = 120;
+L = 12;
+N = 160;
 
 % Measure and Gate
 measure = Measure.AvgFidelity;
-Gate = Gates.GateOfOne;
+Gate = Gates.Xgate;
 
-% Static parameters
+%% Static parameters
+
+% Hadamard values
 % epsilon = 2.4210e+00;
 % omegaX = 3.1426e-16;
-% omegaY = 1.6971e+00;
+% omegaY = -1.6971e+00;
+
+% X gate values
+epsilon = 2.460987392298953;
+omegaX = 0.000091166808424;
+omegaY = 5.017927547094168;
 lambda = 0.0;
 
-epsilon = 1;
-omegaX = 1;
-omegaY = 1;
+% Tautological 1 gate values
+% epsilon = 1;
+% omegaX = 1;
+% omegaY = 1;
 
 % Setup space
 parameter = linspace(-L/2,L/2,N);  
 
 Room = zeros(N,N);
 
+%% Finding the room
 parfor m = 1:N
     m
     % Hamiltonian
@@ -34,7 +43,7 @@ parfor m = 1:N
     Hamilt.Measure = measure;
 
     for n = 1:N
-        para = [parameter(n), parameter(m), omegaY];
+        para = [epsilon, parameter(n),parameter(m)]; %#ok<PFBNS> 
         Hamilt.Parameters = para;
 
         Room(m,n) = MeasureDiffGeneral(Hamilt, Gate = Gate);
@@ -42,17 +51,17 @@ parfor m = 1:N
     end
 end
 
-% Plot room
+%% Plot room
 figure
 pcolor(parameter, parameter, Room)
-xlabel('Epsilon')
-ylabel('Omega X')
+xlabel('Omega X')
+ylabel('Omega Y')
 colorbar
 
 figure
 surf(parameter, parameter, Room)
-xlabel('Epsilon')
-ylabel('Omega X')
+xlabel('Omega X')
+ylabel('Omega Y')
 
 % Finds and prints lowest value with parameters
 [M, I] = min(Room,[],'all','linear');
